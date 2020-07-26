36d4704893dc594cfd03f6afc1711744de485387
Andrew Flik <41635993+andrewflik@users.noreply.github.com>
2020-07-26 17:41:58 +0530

    Updated

---
+++ nn.py
@@ -0,0 +1,104 @@
+"""
+    Author - Devesh
+    1D CNN , 4 CN Layers and 2 FC Layers
+"""
+from keras.preprocessing.image import ImageDataGenerator
+
+# number of images to feed into the NN for every batch
+batch_size = 128
+pic_size = 64
+base_path = "C:/Users/loner/Desktop/RK304/public/"
+datagen_train = ImageDataGenerator()
+datagen_validation = ImageDataGenerator()
+
+train_generator = datagen_train.flow_from_directory(base_path + "train/",
+                                                    target_size=(pic_size,pic_size),
+                                                    color_mode="grayscale",
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical',
+                                                    shuffle=True)
+
+
+validation_generator = datagen_validation.flow_from_directory(base_path + "validation/",
+                                                    target_size=(pic_size,pic_size),
+                                                    color_mode="grayscale",
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical',
+                                                    shuffle=False)
+
+
+from keras.layers import Dense, Input, Dropout, GlobalAveragePooling2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D
+from keras.models import Model, Sequential
+from keras.optimizers import Adam	# OPTIMIZER 
+
+# number of possible label values
+nb_classes = 3	# Facebook/Flicker/Twitter
+
+# Initialising the CNN
+model = Sequential()
+
+# 1 - Convolution
+model.add(Conv2D(64,(3,3), padding='same', input_shape=(pic_size, pic_size,1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 2nd Convolution layer
+model.add(Conv2D(128,(5,5), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 3rd Convolution layer
+model.add(Conv2D(512,(3,3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# 4th Convolution layer
+model.add(Conv2D(512,(3,3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# Flattening
+model.add(Flatten())
+
+# Fully connected layer 1st layer
+model.add(Dense(256))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+# Fully connected layer 2nd layer
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(nb_classes, activation='softmax'))
+
+opt = Adam(lr=0.0001)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+epochs = 50
+
+from keras.callbacks import ModelCheckpoint
+
+checkpoint = ModelCheckpoint("model_weights.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+history = model.fit_generator(generator=train_generator,
+                                steps_per_epoch=train_generator.n//train_generator.batch_size,
+                                epochs=epochs,
+                                validation_data = validation_generator,
+                                validation_steps = validation_generator.n//validation_generator.batch_size,
+                                callbacks=callbacks_list
+                                )
+       
+
